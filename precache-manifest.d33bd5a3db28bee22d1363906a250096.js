004b70f828319e00657ddbeffd112407fcc84995
getravi <getravi@gmail.com>
2020-09-05 21:41:42 -0700

    Updates

---
+++ precache-manifest.d33bd5a3db28bee22d1363906a250096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58980b16abc02ba7f67ed61a9fbb2efe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "da4121a14a0cfe1c80c0",
+    "url": "/monsters-rolodex/static/css/main.6fc57003.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4121a14a0cfe1c80c0",
+    "url": "/monsters-rolodex/static/js/main.985df461.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
